4f4889b06b3cd1aefd8cee4641c5f4f63772c1e2
SivanC <sivan.cooperman@bush.edu>
2017-03-24 20:43:51 -0700

    Quick sort

--- Advanced CS/sortfunctions.py
+++ Advanced CS/sortfunctions.py
@@ -10,3 +10,50 @@ def splitList(aList):
         cList = aList[len(aList)/2:]
         
     return splitList(bList), splitList(cList)
+    
+def mergeSort(alist):       #trinket version
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+            
+def quickSort(aList):
+    
+    if len(aList) <= 1:
+        return aList
+    
+    pivot = aList[0]
+    
+    for i in range(len(aList)):
+        if aList[i] < pivot:
+            print aList[i]
+            aList.insert(0, aList.pop(aList[i]))
+
+        else:
+            pass
+    return aList
