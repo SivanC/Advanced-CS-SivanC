73ba730783da2a51ed96b7fbdae83a2e0236c466
SivanC <sivan.cooperman@bush.edu>
2017-03-27 15:31:14 -0700

    Quick Sort Update (Broken but Progressing)
    Will work on tonight (3.27)

--- Advanced CS/sortfunctions.py
+++ Advanced CS/sortfunctions.py
@@ -1,5 +1,6 @@
 import pdb
 import random
+import cProfile
 
 def swapVal(value1, value2):
     tempVal = 0
@@ -62,25 +63,38 @@ def quickSort(aList):
     print aList
     if len(aList) <= 1:
         return aList
-        
-    pivot = aList[0]
+    
+    pivotIndex = 0    
+    pivot = aList[pivotIndex]
     smallNum = len(aList) - 1
     bigNum = 0
     
     while bigNum < smallNum:
-        
+        print 'Big is less than small'
+        print 'pivot is at ' + str(pivotIndex)
         while aList[bigNum] < pivot:      #checks for a number greater than or equal to the pivot starting from the first index of the array
             print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
+            bigNum += 1
             if bigNum < len(aList):
-                bigNum += 1
+                print 'bigNum reset'
+                bigNum = 0
+                pivotIndex += 1
+                break
+                
             
-        while aList[smallNum] >= pivot:   #checks for a numebr less than the pivot starting from the last index of the array
+        while aList[smallNum] >= pivot:   #checks for a number less than the pivot starting from the last index of the array
             print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
             smallNum -= 1
             if smallNum < 0:
-                smallNum = 0
-                
-        aList[bigNum] = swapVal(aList[bigNum], aList[smallNum])[0]       #swaps the values of aList[bigNum] and aList[smallNum]
-        aList[smallNum] = swapVal(aList[bigNum], aList[smallNum])[1]
-        
+                print 'smallNum reset'
+                smallNum = len(aList) - 1
+                pivotIndex += 1
+                break      
+                  
+        swappedVals = swapVal(aList[bigNum], aList[smallNum])
+        aList[bigNum] = swappedVals[0]       #swaps the values of aList[bigNum] and aList[smallNum]
+        aList[smallNum] = swappedVals[1]
+        print 'swapping ' + str(swappedVals)
     return aList
+    
+#cProfile.run('quickSort(randListGen(10,10))', 'sortfunctions Profile')
