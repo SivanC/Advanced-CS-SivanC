4a7b5b54d20670ff264ede3d3307e1d22145ace4
SivanC <sivan.cooperman@bush.edu>
2017-03-28 09:45:37 -0700

    Fixed quickSort
    Added (broken) logs, quickSort can now separate lists based on pivot
    accurately.

--- Advanced CS/sortfunctions.py
+++ Advanced CS/sortfunctions.py
@@ -22,7 +22,6 @@ def randListGen(listLength, listRange):
 def splitList(aList):
     """Separates all items in a list
     """
-    
     if len(aList) <= 1:
         return aList #if the list is one index long return it
 
@@ -74,12 +73,15 @@ def quickSort(aList): #Best case is 1 swap needed to sort the list. Worst case i
     smallNum = len(aList) - 1
     bigNum = 1
     
-    while bigNum < smallNum: #If this condition is fulfilled then there are only numbers smaller than the pivot in the first half, and only numbers larger in the second, enabling recursive sorting
+    while bigNum + 1 < smallNum: #If this condition is fulfilled then there are only numbers smaller than the pivot in the first half, and only numbers larger in the second, enabling recursive sorting
         pivot = aList[pivotIndex] #Defined inside the while loop in order to be updated if all numbers are larger than the pivot
-        print 'Big is less than small'
+        swapAllowSmall = False    #resetting prerequisites for swapping
+        swapAllowBig = False
         print 'pivot is at ' + str(pivotIndex)
+        print 'pivot value is ' + str(pivot)
+        print 'bigNum is ' + str(bigNum)
         print 'aList[bigNum] is ' + str(aList[bigNum])
-        while aList[bigNum] < pivot and  bigNum < smallNum:      #checks for a number greater than or equal to the pivot starting from the first index of the array
+        while aList[bigNum] < pivot:      #checks for a number greater than or equal to the pivot starting from the first index of the array
             bigNum += 1
             print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
             if bigNum > len(aList):
@@ -87,39 +89,29 @@ def quickSort(aList): #Best case is 1 swap needed to sort the list. Worst case i
                 pivotIndex += 1
                 bigNum = pivotIndex + 1
                 break
-        
-        #about half the time turns into infinite loop between two numbers 
-        while aList[smallNum] >= pivot and smallNum > (bigNum + 1):   #checks for a number less than the pivot starting from the last index of the array
-            smallNum -= 1
-            print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
-            if smallNum < 0:
-                print 'smallNum reset'
+        if aList[bigNum] >= pivot: #swapAllowBig inside a conditional so that it is not triggered by the break if bigNum > len(aList)
+            swapAllowBig = True
+            
+        while aList[smallNum] >= pivot:   #checks for a number less than the pivot starting from the last index of the array
+            if smallNum > bigNum + 1:               
+                smallNum -= 1
+                print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
+            else:
+                print 'No numbers smaller than pivot found! Adjusting pivot...'
                 smallNum = len(aList) - 1
                 pivotIndex += 1
-                bigNum += 1 # updated to start comparisons with the number in front of the pivot
+                bigNum = pivotIndex
                 break
-                  
-        swappedVals = swapVal(aList[bigNum], aList[smallNum])
-        aList[bigNum] = swappedVals[0]       #swaps the values of aList[bigNum] and aList[smallNum]
-        aList[smallNum] = swappedVals[1]
-        print 'swapping ' + str(swappedVals)
+        if aList[smallNum] < pivot:
+            swapAllowSmall = True
+            
+        if swapAllowBig == True and swapAllowSmall == True:
+            swappedVals = swapVal(aList[bigNum], aList[smallNum])
+            aList[bigNum] = swappedVals[0]       #swaps the values of aList[bigNum] and aList[smallNum]
+            aList[smallNum] = swappedVals[1]
+            print 'swapping ' + str(swappedVals)
+            
+    
     return aList
     
-#cProfile.run('quickSort(randListGen(10,10))', 'sortfunctions Profile')
- #=======
- #               smallNum = 0
- #             
- #       aList[bigNum] = swapVal(aList[bigNum], aList[smallNum])[0]       #swaps the values of aList[bigNum] and aList[smallNum]
- #       aList[smallNum] = swapVal(aList[bigNum], aList[smallNum])[1]#
- #
- #       print aList[bigNum]
- #       print aList[smallNum]
- #
- #   return aList
-
-#samiList = randListGen(10, 8)
-#print "My List: " + str(samiList)
-
-#mergeSort(samiList)
-
-#print "Quick Sorted: " + str(quickSort(samiList))
+cProfile.run('quickSort(randListGen(10,10))','C:\Users\Sivan\Documents\GitHub\Advanced-CS-SivanC\Advanced CS\sortfunctions_Profile.txt')
