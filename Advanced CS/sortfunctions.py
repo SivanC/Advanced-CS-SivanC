5afdde3637d15164824f4735823f29014438da3e
SivanC <sivan.cooperman@bush.edu>
2017-03-27 18:10:58 -0700

    Sort Functions Progress
    Updated Quick Sort to be marginally useful, still some bugs that I need
    to figure out. Bug where occasionally the loop will get stuck forever
    swapping two numbers, most likely related to smallNum while conditional
    and bigNum if statement inside while loop to avert list index error.

--- Advanced CS/sortfunctions.py
+++ Advanced CS/sortfunctions.py
@@ -1,8 +1,10 @@
-import pdb
-import random
+import pdb          #python debugger, allows to go through a function step by step using pdb.Trace()
+import random       #used in randListGen()
 import cProfile
 
 def swapVal(value1, value2):
+    """ Swaps the values held in two variables and returns a list with the two swapped values
+    """
     tempVal = 0
     tempVal = value1
     value1 = value2
@@ -10,6 +12,8 @@ def swapVal(value1, value2):
     return value1, value2
     
 def randListGen(listLength, listRange):
+    """ Creates a list with random integers in range listRange and length listLength
+    """
     randList = []
     for i in range(listLength):
         randList.append((random.randint(0, listRange)))
@@ -60,37 +64,40 @@ def mergeSort(alist):       #trinket version
             k=k+1
     print alist
 
-def quickSort(aList):
-    if len(aList) <= 1:
+def quickSort(aList): #Best case is 1 swap needed to sort the list. Worst case is...however many splits of the list are required until there's only one item in each list
+    print aList
+    
+    if len(aList) <= 1:   #checking the list isn't unsortable
         return aList
     
-    pivotIndex = 0    
-    pivot = aList[pivotIndex]
+    pivotIndex = 0 #setting the default pivot of aList[0]
     smallNum = len(aList) - 1
-    bigNum = 0
+    bigNum = 1
     
-    while bigNum < smallNum:
+    while bigNum < smallNum: #If this condition is fulfilled then there are only numbers smaller than the pivot in the first half, and only numbers larger in the second, enabling recursive sorting
+        pivot = aList[pivotIndex] #Defined inside the while loop in order to be updated if all numbers are larger than the pivot
         print 'Big is less than small'
         print 'pivot is at ' + str(pivotIndex)
-        while aList[bigNum] < pivot:      #checks for a number greater than or equal to the pivot starting from the first index of the array
-            print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
+        print 'aList[bigNum] is ' + str(aList[bigNum])
+        while aList[bigNum] < pivot and  bigNum < smallNum:      #checks for a number greater than or equal to the pivot starting from the first index of the array
             bigNum += 1
-            if bigNum < len(aList):
+            print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
+            if bigNum > len(aList):
                 print 'bigNum reset'
-                bigNum = 0
                 pivotIndex += 1
+                bigNum = pivotIndex + 1
                 break
-                
-            
-        while aList[smallNum] >= pivot:   #checks for a number less than the pivot starting from the last index of the array
-            print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
+        
+        #about half the time turns into infinite loop between two numbers 
+        while aList[smallNum] >= pivot and smallNum > (bigNum + 1):   #checks for a number less than the pivot starting from the last index of the array
             smallNum -= 1
+            print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
             if smallNum < 0:
-<<<<<<< HEAD
                 print 'smallNum reset'
                 smallNum = len(aList) - 1
                 pivotIndex += 1
-                break      
+                bigNum += 1 # updated to start comparisons with the number in front of the pivot
+                break
                   
         swappedVals = swapVal(aList[bigNum], aList[smallNum])
         aList[bigNum] = swappedVals[0]       #swaps the values of aList[bigNum] and aList[smallNum]
@@ -99,21 +106,20 @@ def quickSort(aList):
     return aList
     
 #cProfile.run('quickSort(randListGen(10,10))', 'sortfunctions Profile')
-=======
-                smallNum = 0
-                
-        aList[bigNum] = swapVal(aList[bigNum], aList[smallNum])[0]       #swaps the values of aList[bigNum] and aList[smallNum]
-        aList[smallNum] = swapVal(aList[bigNum], aList[smallNum])[1]
+ #=======
+ #               smallNum = 0
+ #             
+ #       aList[bigNum] = swapVal(aList[bigNum], aList[smallNum])[0]       #swaps the values of aList[bigNum] and aList[smallNum]
+ #       aList[smallNum] = swapVal(aList[bigNum], aList[smallNum])[1]#
+ #
+ #       print aList[bigNum]
+ #       print aList[smallNum]
+ #
+ #   return aList
 
-        print aList[bigNum]
-        print aList[smallNum]
+#samiList = randListGen(10, 8)
+#print "My List: " + str(samiList)
 
-    return aList
+#mergeSort(samiList)
 
-samiList = randListGen(10, 8)
-print "My List: " + str(samiList)
-
-mergeSort(samiList)
-
-print "Quick Sorted: " + str(quickSort(samiList))
->>>>>>> origin/master
+#print "Quick Sorted: " + str(quickSort(samiList))
