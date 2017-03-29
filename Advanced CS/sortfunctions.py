1048e6e97d9c774181ff9a2576a226cfd628bdf8
SivanC <sivan.cooperman@bush.edu>
2017-03-29 11:49:00 -0700

    Quick Sort Finished (Minus Recursion)
    quickSort() will now split lists into numbers lower than and numbers
    higher than the pivot and place the pivot in the middle. Created
    function isSorted() which determines whether a list is sorted by using
    comparison.

--- Advanced CS/sortfunctions.py
+++ Advanced CS/sortfunctions.py
@@ -2,6 +2,8 @@ import pdb          #python debugger, allows to go through a function step by st
 import random       #used in randListGen()
 import cProfile
 
+
+
 def swapVal(value1, value2):
     """ Swaps the values held in two variables and returns a list with the two swapped values
     """
@@ -63,41 +65,49 @@ def mergeSort(alist):       #trinket version
             k=k+1
     print alist
 
+def isSorted(aList):
+    """Checks if a list is sorted by comparing each entry to the next
+    """
+    index = 0
+    while aList[index] < aList[index + 1]:
+        index += 1
+    if index == len(aList):
+        return True
+    else:
+        return False
+
 def quickSort(aList): #Best case is 1 swap needed to sort the list. Worst case is...however many splits of the list are required until there's only one item in each list
     print aList
-    
+    print 'pivot is ' + str(aList[0])
     if len(aList) <= 1:   #checking the list isn't unsortable
         return aList
     
     pivotIndex = 0 #setting the default pivot of aList[0]
     smallNum = len(aList) - 1
     bigNum = 1
+    isSorted = False
     
     while bigNum + 1 < smallNum: #If this condition is fulfilled then there are only numbers smaller than the pivot in the first half, and only numbers larger in the second, enabling recursive sorting
         pivot = aList[pivotIndex] #Defined inside the while loop in order to be updated if all numbers are larger than the pivot
         swapAllowSmall = False    #resetting prerequisites for swapping
         swapAllowBig = False
-        print 'pivot is at ' + str(pivotIndex)
-        print 'pivot value is ' + str(pivot)
-        print 'bigNum is ' + str(bigNum)
-        print 'aList[bigNum] is ' + str(aList[bigNum])
         while aList[bigNum] < pivot:      #checks for a number greater than or equal to the pivot starting from the first index of the array
             bigNum += 1
-            print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
-            if bigNum > len(aList):
-                print 'bigNum reset'
+            if bigNum >= len(aList):
+                #print 'bigNum reset'
                 pivotIndex += 1
                 bigNum = pivotIndex + 1
                 break
+            #print 'bigNum is ' + str(bigNum) + ' and the value is ' + str(aList[bigNum])
         if aList[bigNum] >= pivot: #swapAllowBig inside a conditional so that it is not triggered by the break if bigNum > len(aList)
             swapAllowBig = True
             
         while aList[smallNum] >= pivot:   #checks for a number less than the pivot starting from the last index of the array
             if smallNum > bigNum + 1:               
                 smallNum -= 1
-                print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
+                #print 'smallNum is ' + str(smallNum) + ' and the value is ' + str(aList[smallNum])
             else:
-                print 'No numbers smaller than pivot found! Adjusting pivot...'
+                #print 'No numbers smaller than pivot found! Adjusting pivot...'
                 smallNum = len(aList) - 1
                 pivotIndex += 1
                 bigNum = pivotIndex
@@ -109,9 +119,18 @@ def quickSort(aList): #Best case is 1 swap needed to sort the list. Worst case i
             swappedVals = swapVal(aList[bigNum], aList[smallNum])
             aList[bigNum] = swappedVals[0]       #swaps the values of aList[bigNum] and aList[smallNum]
             aList[smallNum] = swappedVals[1]
-            print 'swapping ' + str(swappedVals)
-            
     
+    middleNum = aList[0] #assigning the number we will move to the middle of the list
+    counter = 1
+    print aList
+    while aList[counter] < middleNum and counter < len(aList) - 1: #determining the divide between numbers lower than and numbers higher than the pivot
+        counter += 1
+    counter -= 1 #setting the counter to the number after which we insert the pivot
+    swappedVals = swapVal(middleNum, aList[counter]) #swapping the pivot with the last value lower than it
+    aList[0] = swappedVals[0]
+    aList[counter] = swappedVals[1]
+    
+
     return aList
-    
-cProfile.run('quickSort(randListGen(10,10))','C:\Users\Sivan\Documents\GitHub\Advanced-CS-SivanC\Advanced CS\sortfunctions_Profile.txt')
+
+#cProfile.run('quickSort(randListGen(10,10))','C:\Users\Sivan\Documents\GitHub\Advanced-CS-SivanC\Advanced CS\sortfunctions_Profile.txt')
