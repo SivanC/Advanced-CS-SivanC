1529598a97c866f9db2d38e1687a01df290c8a44
SivanC <sivan.cooperman@bush.edu>
2017-04-28 18:34:14 -0700

    Parser working(?) but computer is very slow

--- Advanced_CS/nGramsParser.py
+++ Advanced_CS/nGramsParser.py
@@ -2,6 +2,20 @@ import linecache as lc
 import matplotlib.pyplot as plt
 import pdb
 
+def getData(line):
+    data = line.split("\t")
+    if '_' in data:
+        temp = data[0].split("_")
+        data[0] = temp[0]
+        print data
+        data.insert(1, temp[1])
+        del(data[1])
+    if '\n' in data:
+        temp = data[3].split("\n")
+        data[3] = temp[0]
+    
+    return data
+
 def nGramsParse():
     wordFreq = []
     yearAxis = []
@@ -22,8 +36,10 @@ def nGramsParse():
     wordFound = False
     lineCount = 0
     while not wordFound: 
+        print 'Searching...'
         lineCount += 1
-        wordData = lc.getline(fileName, lineCount).split("    ")
+        wordData = getData(lc.getline(fileName, lineCount))
+        print 'Current word: ' + str(wordData)
         if word == wordData[0]:
             wordFound = True
             
@@ -31,6 +47,7 @@ def nGramsParse():
         print 'That word is not in our data!'
         return
     
+    print 'Word found! Calculating usage...'
     currentYear = wordData[1]
     
     while wordData[1] <= currentYear:
@@ -38,6 +55,7 @@ def nGramsParse():
         wordFreq.append(wordData[2])
         yearAxis.append(wordData[1])
         lineCount += 1
-        
+    
+    plt.xticks(int(currentYear) - 1, currentYear + lineCount)
     plt.plot(wordFreq, yearAxis)
     plt.show(block=True)
--- Advanced_CS/profiletesting.py
+++ Advanced_CS/profiletesting.py
@@ -1,14 +1,14 @@
 import cProfile, pstats
-from sortfunctions import insertSort
+import nGramsParser
+reload(nGramsParser)
 
 profile = cProfile.Profile() #Creating the profile class
 sortMethod = 'cumtime' #Defining filter for data
-outputPath = '/Users/Sivan/Advanced-CS-SivanC/Advanced CS/profileTestingOutput.txt'
+outputPath = '/Users/Sivan/Documents/Github/Advanced-CS-SivanC/Advanced_CS/profileTestingOutput.txt'
 output = open(outputPath, 'w') #Opening the output file for writing
 
 profile.enable() #Starting tracking of data
-for i in range(50):
-	insertSort([9,8,7,6,5,4,3,2,1])
+nGramsParser.nGramsParse()
 profile.disable #Ending tracking of data
 
 stats = pstats.Stats(profile, stream=output).strip_dirs().sort_stats(sortMethod).print_stats() #Creating a stats printout from the data of our profile and writing it to the output file without directories, and sorted by cumulative time.
