fa96bc488bd2876b1c348783554b8ba9f27ddfe0
SivanC <sivan.cooperman@bush.edu>
2017-05-01 15:21:47 -0700

    nGramsParser Fixes and Updates

--- Advanced_CS/nGramsParser.py
+++ Advanced_CS/nGramsParser.py
@@ -1,3 +1,10 @@
+# File: nGramsParser.py
+# Versions: Python 2.7.13
+# Name: Sivan Cooperman
+# Date: 5.1.2017
+# Desc: Parses Google nGrams Data to display usage statistics
+# Usage: Requires a file to parse path and the word you would like to find as raw input after instantiating the function
+
 import linecache as lc
 import matplotlib.pyplot as plt
 import pdb
@@ -10,7 +17,7 @@ def getData(line):
         print data
         data.insert(1, temp[1])
         del(data[1])
-    if '\n' in data:
+    if '\n' in data[3]:
         temp = data[3].split("\n")
         data[3] = temp[0]
     
@@ -39,9 +46,12 @@ def nGramsParse():
         print 'Searching...'
         lineCount += 1
         wordData = getData(lc.getline(fileName, lineCount))
+        if wordData[0] == ' ':
+            break
         print 'Current word: ' + str(wordData)
         if word == wordData[0]:
             wordFound = True
+        lc.clearcache()
             
     if not wordFound:
         print 'That word is not in our data!'
@@ -51,10 +61,11 @@ def nGramsParse():
     currentYear = wordData[1]
     
     while wordData[1] <= currentYear:
-        wordData = lc.getline(fileName, lineCount).split("    ")
+        wordData = getData(lc.getline(fileName, lineCount))
         wordFreq.append(wordData[2])
         yearAxis.append(wordData[1])
         lineCount += 1
+        lc.clearcache()
     
     plt.xticks(int(currentYear) - 1, currentYear + lineCount)
     plt.plot(wordFreq, yearAxis)
